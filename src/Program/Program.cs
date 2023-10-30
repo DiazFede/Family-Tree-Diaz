d3aa639aff3aebb946d195adc6899876997ee0b6
DiazFede <diazfedericoucu@gmail.com>
2023-10-30 09:59:33 -0300

    Programm

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,7 +7,7 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Person Abuelo = new Person("Abuelo", 85);                        Person Abuelo = new Person("Abuelo", 75);
+            Person Abuelo = new Person("Abuelo", 85);                        
             Person Abuela = new Person("Abuela", 88);
             Person Madre = new Person("Madre", 65);
             Person Padre = new Person("Padre", 60);
@@ -15,14 +15,23 @@ namespace Program
             Person Hija = new Person("Hija", 34);
             Person Nieta = new Person("Nieta", 5);
             Person Nieto = new Person("Nieto", 3);
-            Node Node = new Node();//nodo hoja
+            
+            Node AbueloNode = new Node(Abuelo);//nodo hoja
+            Node AbuelaNode = new Node(Abuela);//nodo hoja
+            Node MadreNode = new Node(Madre);
+            Node PadreNode = new Node (Padre);
+            Node HijoNode = new Node (Hijo);
+            Node HijaNode = new Node(Hija);
+            Node NietaNode= new Node (Nieta);
+            Node NietoNode= new Node (Nieto);
+
            
 
-            Abuelo.AddChildren(Padre);
-            Abuela.AddChildren(Madre);
-            Padre.AddChildren(Hijo);
-            Madre.AddChildren(Hija);
-            Hijo.AddChildren(Nieto);
+            AbueloNode.AddChildren(PadreNode);
+            AbuelaNode.AddChildren(MadreNode);
+            PadreNode.AddChildren(HijoNode);
+            MadreNode.AddChildren(HijaNode);
+            HijoNode.AddChildren(NietoNode);
 
             // visitar el árbol aquí
 
