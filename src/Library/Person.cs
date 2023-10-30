f936934bf913d23f66895a3217ea61cf0b3fd144
DiazFede <diazfedericoucu@gmail.com>
2023-10-30 09:24:29 -0300

    Person

---
+++ src/Library/Person.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System;
+
+namespace Library
+{
+    public class Person
+    {
+        public string Name {get;}
+        public int Age {get;}
+
+        public Person (string name, int age)
+        {
+            this.Name=name;
+            this.Age=age;
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+}
